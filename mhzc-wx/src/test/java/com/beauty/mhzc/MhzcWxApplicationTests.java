7ed1cb7f2a8cb8c88cabd12ddcec43ad4c4db34f
zx2233 <1035554874@qq.com>
2020-12-20 17:09:42 +0800

    增加微信模块（微信小程序调用接口）

---
+++ mhzc-wx/src/test/java/com/beauty/mhzc/MhzcWxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beauty.mhzc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhzcWxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
